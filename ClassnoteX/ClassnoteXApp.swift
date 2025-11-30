59373601225ec9602a6f53b6f0a90a5b9c8f0bc7
FORIFOR <horio.shuhei98@gmail.com>
2025-11-30 22:27:00 +0900

    Initial Commit

---
+++ ClassnoteX/ClassnoteXApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassnoteXApp.swift
+//  ClassnoteX
+//
+//  Created by 堀尾周平 on 2025/11/30.
+//
+
+import SwiftUI
+
+@main
+struct ClassnoteXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClassnoteX/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClassnoteX
+//
+//  Created by 堀尾周平 on 2025/11/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
